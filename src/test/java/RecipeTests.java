e9c40360549e95d0d3cc78af1615be4a901f0197
g.solovev <116116Ss>
2022-09-01 12:23:58 +0300

    Добавил asserts

--- src/test/java/RecipeTests.java
+++ src/test/java/RecipeTests.java
@@ -4,25 +4,24 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonStringFormatVisitor;
+import groovy.json.JsonOutput;
 import io.restassured.RestAssured;
 import io.restassured.builder.ResponseBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.specification.ResponseSpecification;
+import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLOutput;
-import java.util.Formatter;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 
 
 public class RecipeTests {
@@ -100,8 +99,8 @@ public class RecipeTests {
 
 
     @JsonIgnoreProperties(ignoreUnknown = true)
-
     @Test
+        //Десерилизация json. Раабота с полями как с объектами
     void Test5() throws JsonProcessingException {
         String body = given()
                 .when()
@@ -112,11 +111,52 @@ public class RecipeTests {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
         Dat data = objectMapper.readValue(body, Dat.class);
-
         System.out.println("Ответ  = " + data.data[2].email);
         System.out.println("Ответ  = " + data.data[2].firstName);
         System.out.println("Ответ  = " + data.support.text);
 
+
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    @Test
+        // Добавил асерты
+    void Test6() throws JsonProcessingException {
+        String firstname = "Tobias";
+        String[] array = new String[]{"ab"};
+        List<Integer> list = Arrays.asList(5, 2, 4);
+        Map<Integer, String> map = new HashMap<Integer, String>();
+        map.put(1, "a");
+        map.put(2, "b");
+        map.put(3, "c");
+        String body = given()
+                .when()
+                .get("api/users?page=2")
+                .then()
+                .extract()
+                .body().asString();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+        Dat data = objectMapper.readValue(body, Dat.class);
+        System.out.println("Ответ  = " + data.data[2].email);
+        System.out.println("Ответ  = " + data.data[2].firstName);
+        System.out.println("Ответ  = " + data.support.text);
+        System.out.println("Ответ  = " + data.total);
+        assertThat(firstname, equalTo(data.data[2].firstName)); // сравнение значения стринг
+        assertThat(data.data[2].firstName, anyOf(containsString("Tob"), endsWith("ias"))); // либо содержит Tob либо кончается на ias
+        //assertThat(data.data[2].firstName, not(containsString("Tob"))); // не содержит
+        assertThat(data.data[2].firstName, allOf(containsString("Tob"), endsWith("ias"))); // и содержит и кончается
+        assertThat(data.total, allOf(greaterThan(5), lessThanOrEqualTo(15), not(equalTo(6))));// больше 5, меньше 15, не равна 6
+        assertThat(list, is(not(empty()))); // проверить, что список не пустой
+        assertThat(list, hasSize(3)); // проверить размер списка
+        assertThat(list, everyItem(greaterThan(0))); // проверить, что все элементы больше 0
+        assertThat(list, hasItem(5)); // проверить, что в списке есть 5
+        assertThat(list, contains(5, 2, 4)); // проверить, что состоит из элементов
+        assertThat(map, hasKey(1)); // содержит ключь
+        assertThat(map, hasValue("a")); // содержит значение
+        assertThat(map, hasEntry(3, "c")); // содержит запись
+        assertThat(array, not(emptyArray()));
+        assertThat(array, arrayWithSize(3));
 
     }
 
